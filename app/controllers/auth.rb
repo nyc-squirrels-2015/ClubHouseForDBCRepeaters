45d7bdb6a78089712f5b60373077821b347c1f7c
bruce wayne <no1_ldc@hotmail.com>
2015-02-18 10:50:09 -0500

    controllers/auth routhing built and fix bug from models user happend while seeding

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,11 +1,37 @@
+
 get '/' do
-  erb :welcome
+   erb :welcome
 end
 
 get '/login' do
-  
+  erb :login
+end
+
+post '/login' do
+  user = User.authenticate(params[:name], params[:password])
+  if user
+    session[:user_id] = user.id
+    redirect '/posts'
+  else
+    redirect '/login'
+  end
 end
 
 get '/signup' do
-  
+  erb :signup
+end
+
+post '/signup' do
+  if params[:password] == params[:password_confirmation]
+    new_user = User.create(name: params[:name], password: params[:password], password_confirmation: params[:password_confirmation])
+    session[:user_id] = new_user.id
+    redirect '/index'
+  else
+    redirect '/signup'
+  end
+end
+
+get '/logout' do
+  session.delete(:user_id)
+  redirect '/'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
 
-def self.authenticate(name, password)
-    auth = User.find_by(name: name).try(:authenticate, password)
+  def self.authenticate(name, password)
+    auth = User.find_by(name :name).try(:authenticate, password)
     auth
   end
 end
