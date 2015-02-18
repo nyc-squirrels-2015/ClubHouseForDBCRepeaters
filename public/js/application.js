eb7876c4c13115447c2ae11aebef2e2c9b7d4998
bruce wayne <no1_ldc@hotmail.com>
2015-02-18 14:43:59 -0500

    first_ajax is add

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,13 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+$(document).ready(function() {
+   $("#login_button").on("click",function(event){
+      event.preventDefault();
+      $.ajax({
+            type:'get',
+            url:'/login'
+        }).success(function(response){
+            $("#login_button").after('<div class="response">' + response + '</div>');
+            $("#login_button").hide();
+        });
+    });
 });
